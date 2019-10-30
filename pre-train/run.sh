28df960ebcc9f70807165c55334d09a3d4ed95c7
chz816 <czh097@gmail.com>
2019-10-30 19:09:34 -0400

    update

--- pre-train/run.sh
+++ pre-train/run.sh
@@ -5,7 +5,7 @@ python create_pretraining_data.py \
   --vocab_file=./vocab.txt \
   --do_lower_case=True \
   --max_seq_length=100 \
-  --max_predictions_per_seq=20 \
+  --max_predictions_per_seq=15 \
   --masked_lm_prob=0.15 \
   --random_seed=12345 \
   --dupe_factor=5
